a4b3e27d2def61c6c5c51a9674226867822097a5
Malin Sundberg <143407879+Malinsund@users.noreply.github.com>
2024-04-05 13:04:27 +0200

    comments in code

--- app/shoppinglist/page.tsx
+++ app/shoppinglist/page.tsx
@@ -4,20 +4,16 @@ import { useState } from "react";
 // Denna komponenten bryter mot Single Responsibility Principle (SRP) då jag inkluderar logik både för att lägga till nya object samt för att rendera listan, för att förbättra borde jag ha delat upp denna i 2 olika komponenter!
 
 interface ShoppingItem {
-  name: string;
-  bought: boolean;
+  name: string; bought: boolean;
 }
 
 function ShoppingList() {
-  const [item, setItem] = useState<string>("");
+  const [thingOnList, setItem] = useState<string>(""); // i denna kodraden kunde jag istället för att döpa saken på listan till "thingOnList" valt att använda mig av t.ex "item" som både är kortare och mer igenkänt när vi skriver kod
   const [shoppingList, setShoppingList] = useState<ShoppingItem[]>([]);
 
-  const addItemToList = () => {
-    if (item.trim() !== "") {
-      setShoppingList([...shoppingList, { name: item, bought: false }]);
-      setItem("");
-    }
+  const addItemToList = () => { if (thingOnList.trim() !== "") { setShoppingList([...shoppingList, { name: thingOnList, bought: false }]); setItem("");}
   };
+  // Här ovan hr jag gkort ett exempel på hur man inte borde göra, hela kodstycket är skrivet på en rad, detta gör koden svårläst och svår att förstå då detta inte är kutym för hur man borde skriva koden, det är bättre att göra som här nedanför.
 
   const toggleBought = (index: number) => {
     const updatedList = [...shoppingList];
@@ -29,24 +25,31 @@ function ShoppingList() {
     <div className="flex flex-col gap-4">
       <input
         type="text"
-        value={item}
+        value={thingOnList}
         onChange={(e) => setItem(e.target.value)}
       />
+      <div>
       <button className="rounded-lg bg-slate-400" onClick={addItemToList}>
         Lägg till
       </button>
-      <ul>
-        {shoppingList.map((item, index) => (
-          <li key={index}>
-            <input
-              type="checkbox"
-              checked={item.bought}
-              onChange={() => toggleBought(index)}
-            />
-            <span>{item.name}</span>
-          </li>
-        ))}
-      </ul>
+      </div> {/* här ligger det divar runt varje element vilket i denna lilla kod är väldigt onödigt då det inte gör någon skillnad på stylingen eller koden utan blir bara onödigt många boxar */}
+      <div>
+        <ul>
+          {shoppingList.map((thingOnList, index) => (
+              <li key={index}>
+
+
+              <input
+                type="checkbox"
+                checked={thingOnList.bought}
+                onChange={() => toggleBought(index)}/* konstiga indragningar och onödiga mellanrum som gör koden ytterligare svårläst *//>
+
+
+          <span>{thingOnList.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
